2371db23ba8640b1d003be02fdd45091f7ad80b7
Michael Mathu <mathumichael17@gmail.com>
2022-01-16 20:01:58 +0300

    hello world contract

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -5,13 +5,32 @@ pragma solidity >= 0.4.0;//compiler instruction, telling the Solidity compiler w
 contract Greeter { //data and functions/methods defined in here will be isolated to this contract 
     string private _greeting = 'Hello, World!'; //State Variable. Stores data in contract's persisted storage which exist for the entire lifetime of our contract.
 
+    address private _owner;
+
+    constructor() public {
+        _owner = msg.sender;
+    }
+    
+    modifier onlyOwner() { //modifiers syntax looks like function but without the visibility declaration
+        require(
+        msg.sender == _owner,
+        "Ownable: caller is not the owner"
+        );
+        _; // where the function that is being modified will be called.
+       }
+
     function greet() external view returns(string memory) {
         return _greeting;
         }  
     
     //setGreeting function updates the state ot our contract with a new greeting
-    function setGreeting(string calldata greeting) external { //string value is expected and will be refferd by indetifier 'greeting'. The being passed in is not part of the contract’s persisted storage, but is included as part of    the calldata and must be labeled with the data location 'calldata'.
+    function setGreeting(string calldata greeting) external onlyOwner { //string value is expected and will be refferd by indetifier 'greeting'. The being passed in is not part of the contract’s persisted storage, but is included as part of    the calldata and must be labeled with the data location 'calldata'.
     _greeting = greeting; 
-
     }
+
+    // Getter function to Add ownership
+    function owner() public view returns(address) {
+        return _owner;
+        }
+       
 }
--- test/greeter_test.js
+++ test/greeter_test.js
@@ -10,7 +10,7 @@
 
 const GreeterContract = artifacts.require("Greeter");
 
-contract("Greeter", () => { 
+contract("Greeter", (accounts) => {  //we pass in the 'accounts' parameter to access to the accounts in out test environment
     it("has been deployed successfully", async () => {
     const greeter = await GreeterContract.deployed();
     assert(greeter, "contract was not deployed");  
@@ -23,18 +23,52 @@ describe("greet()", () => {
     const expected = "Hello, World!";
     const actual = await greeter.greet();
     assert.equal(actual, expected, "greeted with 'Hello, World!'");
-    });
-});   
+    });     
+});
 
-contract("Greeter: update greeting", () => {
+describe("owner()", () => {
+    it("returns the address of the owner", async () => {
+    const greeter = await GreeterContract.deployed();
+    const owner = await greeter.owner();
+    assert(owner, "the current owner"); // Is the owner address is the same as the deploy‐ing address?
+    });
+
+    it("matches the address that originally deployed the contract", async () => {
+        const greeter = await GreeterContract.deployed();
+        const owner = await greeter.owner();
+        const expected = accounts[0];
+        assert.equal(owner, expected, "matches address used to deploy contract");
+        });       
+});
+
+// MAKING THE CONTRACT DYNAMIC
+contract("Greeter: update greeting", (accounts) => {
     describe("setGreeting(string)", () => {
+        describe("when message is sent by the owner", () => {
         it("sets greeting to passed in string", async () => {
         const greeter = await GreeterContract.deployed()
-        const expected = "Hi there!"; //we set a variable to hold our expected return value
+        const expected = "The owner changed the message"; //we set a variable to hold our expected return value
         await greeter.setGreeting(expected); //passing the expected return value string
         const actual = await greeter.greet(); //Update the greeting and ask the greet for its return value. The call is asynchronous thus the await keyword
         assert.equal(actual, expected, "greeting was not updated"); //check the value from greet against our expected value
         });
     });
 });
+
+describe("when message is sent by another account", () => {
+    it("does not set the greeting", async () => {
+    const greeter = await GreeterContract.deployed()
+    const expected = await greeter.greet();
+    try {
+        await greeter.setGreeting("Not the owner", { from: accounts[1] });
+        } catch(err) {
+        const errorMessage = "Ownable: caller is not the owner"
+        assert.equal(err.reason, errorMessage, "greeting should not update");
+        return;
+        }
+        assert(false, "greeting should not update");
+        });
+        });
+    });
+       
    
